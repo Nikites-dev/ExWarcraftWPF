4649ba34b95ba33c9147f1b347937b94ec3c9b39
Nikites <rockstar.gg@mail.ru>
2022-09-11 19:44:36 +0300

    Add project files.

---
+++ ExWarcraftWPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExWarcraftWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Button btn = new Button();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bool isEvent = false;
+            btn = (Button)sender;
+
+            MessageBox.Show(Convert.ToString(btn.Name));
+
+        }
+
+    }
+}
